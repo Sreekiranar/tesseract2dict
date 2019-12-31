0c64756cdcf5cf1e5219153747a363aeaebc6dec
Sreekiran AR <sreekiran.ar@bridgei2i.com>
2019-12-31 15:44:10 +0530

    try except added

--- tesseract2dict/tess2dict.py
+++ tesseract2dict/tess2dict.py
@@ -28,23 +28,26 @@ class TessToDict:
 			>>> word_dict=td.tess2dict(inputImage,'out','outfolder')
 
 		"""
-		### temp file creation for tesseract call
-		directory=MAKETREEDIR()
-		directory.makedir('tmp')
-		cv2.imwrite('tmp/hocr.jpg',image)
+		try:
+			### temp file creation for tesseract call
+			directory=MAKETREEDIR()
+			directory.makedir('tmp')
+			cv2.imwrite('tmp/hocr.jpg',image)
+			if outpath not in ['.','',' ']:
+				directory.makedir(outpath)
+			###call tesseract to get hocr out
+			pytesseract.pytesseract.run_tesseract(
+			'tmp/hocr.jpg', '{}'.format(os.path.join(outpath,outname)),
+			extension='.html', lang='eng', config="hocr {}".format(config))
+			### removing temp files
+			os.remove("tmp/hocr.jpg")
+			os.rmdir('tmp')
 
-		directory.makedir(outpath)
-		###call tesseract to get hocr out
-		pytesseract.pytesseract.run_tesseract(
-		'tmp/hocr.jpg', '{}'.format(os.path.join(outpath,outname)),
-		extension='.html', lang='eng', config="hocr {}".format(config))
-		### removing temp files
-		os.remove("tmp/hocr.jpg")
-		os.rmdir('tmp')
-
-		### hocr to word_dict
-		word_dict= self.hocr2dict(outpath,outname)
-
+			### hocr to word_dict
+			word_dict= self.hocr2dict(outpath,outname)
+		except Exception as e:
+			print(e)
+			word_dict=pd.DataFrame(columns=['x','y','w','h','text','conf'])
 		return word_dict
 
 	def hocr2dict(self,outpath,name):
@@ -57,26 +60,30 @@ class TessToDict:
 		Returns:
 			dict: word_dict containing coordinates and content of each word
 		"""
-		### read hocr
-		with open(os.path.join(outpath,name+'.hocr'),mode='r',encoding="utf8") as f:
-			hout=f.read()
+		try:
+			### read hocr
+			with open(os.path.join(outpath,name+'.hocr'),mode='r',encoding="utf8") as f:
+				hout=f.read()
 
-		### parsing and converting to word dict
-		parsed_html = BeautifulSoup(hout,"html.parser")
-		words=parsed_html.body.find_all('span',attrs={'class':'ocrx_word'})
-		word_dict=pd.DataFrame()
+			### parsing and converting to word dict
+			parsed_html = BeautifulSoup(hout,"html.parser")
+			words=parsed_html.body.find_all('span',attrs={'class':'ocrx_word'})
+			word_dict=pd.DataFrame()
 
-		for index,word in enumerate(words):
-			bbox,conf=word['title'].split(';')
-			x,y,x2,y2=bbox.split(' ')[1:]
-			confidence=conf.split(' ')[-1]
-			text=word.text
-			w=int(x2)-int(x)
-			h=int(y2)-int(y)
-			word_dict.loc[index,'x']=int(x)
-			word_dict.loc[index,'y']=int(y)
-			word_dict.loc[index,'w']=int(w)
-			word_dict.loc[index,'h']=int(h)
-			word_dict.loc[index,'text']=text
-			word_dict.loc[index,'conf']=int(confidence)
+			for index,word in enumerate(words):
+				bbox,conf=word['title'].split(';')
+				x,y,x2,y2=bbox.split(' ')[1:]
+				confidence=conf.split(' ')[-1]
+				text=word.text
+				w=int(x2)-int(x)
+				h=int(y2)-int(y)
+				word_dict.loc[index,'x']=int(x)
+				word_dict.loc[index,'y']=int(y)
+				word_dict.loc[index,'w']=int(w)
+				word_dict.loc[index,'h']=int(h)
+				word_dict.loc[index,'text']=text
+				word_dict.loc[index,'conf']=int(confidence)
+		except Exception as e:
+			print(e)
+			word_dict=pd.DataFrame(columns=['x','y','w','h','text','conf'])
 		return word_dict
